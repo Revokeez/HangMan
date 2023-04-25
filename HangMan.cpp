d59f35cce7f778a432f7cc49dc65e90146868412
Jonuel Santana <Benitezaliceaj@hotmail.com>
2023-04-25 18:17:39 -0400

    Add colors

--- HangMan.cpp
+++ HangMan.cpp
@@ -1,174 +1,13 @@
-
-#include <iostream>
-#include <Windows.h>
-#include <string>
-
-using namespace std;
-
-#define color SetConsoleTextAttribute
-
-/*bool gameRunning = true;
-string words[3] = { "house", "mirror", "floor" };
-string selectedWord;
-bool guessedLetters[10];
-int guessCount = 0;
-
-void hangman1() {
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    color(hConsole, 7);
-    cout << R"(
-
-                      ___  
-                      |  
-                      |  
-                      |   
-                      |   
-                      |__  
-    
-)";
-}
-void hangman2() {
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    color(hConsole, 2);
-    cout << R"(
-                      ___  
-                      |  |  
-                      |  
-                      |  
-                      |  
-                      |__  
-    
-)";
-}
-void hangman3() {
-    system("cls");
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    color(hConsole, 6);
-    cout << R"(
-                      ___  
-                      |  |  
-                      |  O  
-                      |  
-                      |   
-                      |__  
-    
-)";
-}
-void hangman4() {
-    system("cls");
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    color(hConsole, 3);
-    cout << R"(
-                      ___  
-                      |  |  
-                      |  O  
-                      |  |  
-                      |   
-                      |__  
-    
-)";
-}
-void hangman5() {
-    system("cls");
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    color(hConsole, 1);
-    cout << R"(
-                      ___  
-                      |  |  
-                      |  O  
-                      | /|\  
-                      |   
-                      |__  
-    
-)";
-}
-void hangman6() {
-    system("cls");
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    color(hConsole, 4);
-    cout << R"(
-                      ___  
-                      |  |  
-                      |  O  
-                      | /|\  
-                      | / \  
-                      |__  
-    
-)";
-
-}
-void ShowDrawings()
-{
-    int rIndex = rng.Next(words.Length);//This
-    selectedWord = words[rIndex];//This will choose a word from the array word.
-    guessedLetters = new bool[selectedWord.Length];
-
-    string words[3] = { "house", "mirror", "floor" };
-
-    srand(time(nullptr));
-    int rIndex = rand() % 3;
-    selectedWord = words[rIndex];
-    guessCount = 0;
-
-    switch (guessCount)
-    {
-    case 0:
-        hangman1();
-        break;
-    case 1:
-        hangman2();
-        break;
-    case 2:
-        hangman3();
-        break;
-    case 3:
-        hangman4();
-        break;
-    case 4:
-        hangman5();
-        break;
-    case 5:
-        hangman6();
-        break;
-    }
-
-    //selectedWord = words[rIndex];
-}
-int main()
-{
-
-    int repetir = 0;
-    string selectedword[5];//Por ahora
-    string playerChoice;//La opcion que el jugador escogio
-    
-
-    do {
-        hangman1();
-        hangman2();
-        hangman3();
-        hangman4();
-        hangman5();
-        hangman6();
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        color(hConsole, 7);
-        repetir++;
-    } while (repetir < 2);
-    hangman6();
-    cout << "WELCOME TO HANGMAN\n";
-    cout << "Wright the letter:";
-    cin >> playerChoice;
-}*/
-
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <string>
-
+#include <Windows.h>
+#include <cctype>
 using namespace std;
+
+#define hand HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE)
+#define color SetConsoleTextAttribute
 
 const int OPPORTUNITIES = 6;
 string words[3] = { "house", "mirror", "floor" };
@@ -184,48 +23,62 @@ void Init() {
     for (int i = 0; i < 6; i++) {
         guessedLetters[i] = false;
     }
+    hand;
+    color(hConsole, 7);
     gallows[0] = "_____ \n"
         "| |   \n"
         "|     \n"
         "|     \n"
         "|     \n"
         "|______ \n";
+    hand;
+    color(hConsole, 2);
     gallows[1] = "_____ \n"
         "| |   \n"
         "| O   \n"
         "|     \n"
         "|     \n"
         "|______ \n";
+    hand;
+    color(hConsole, 6);
     gallows[2] = "_____  \n"
         "| |  \n"
         "| O  \n"
         "| |  \n"
         "|    \n"
         "|______ \n";
+    hand;
+    color(hConsole, 3);
     gallows[3] = "_____  \n"
         "| |  \n"
         "| O  \n"
         "|/|  \n"
         "|    \n"
         "|______ \n";
+    hand;
+    color(hConsole, 1);
     gallows[4] = "_____  \n"
         "| |  \n"
         "| O  \n"
         "|/|\\ \n"
         "|    \n"
         "|______ \n";
+    hand;
+    color(hConsole, 4);
     gallows[5] = "_____  \n"
         "| |  \n"
         "| O  \n"
         "|/|\\ \n"
         "|/   \n"
         "|______ \n";
+    hand;
+    color(hConsole, 5);
     gallows[6] = "_____  \n"
         "|  |  \n";
         "|  O  \n";
         "| /|\\  \n";
         "| / \\  \n";
-        "|______"
+        "|______";
 
 }
 void ShowGameStartScreen() {
